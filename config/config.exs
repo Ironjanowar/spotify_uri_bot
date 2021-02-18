ce783037e5561cc8a1c44553ece01a471daee238
Ironjanowar <ironjanowar@gmail.com>
2021-02-18 18:37:52 +0100

    Add json logger file backend

--- config/config.exs
+++ config/config.exs
@@ -25,14 +25,11 @@ config :spotify_uri_bot, SpotifyUriBot.Scheduler,
 config :logger,
   level: :debug,
   truncate: :infinity,
-  backends: [{LoggerFileBackend, :debug}, {LoggerFileBackend, :error}]
+  backends: [{LoggerJSONFileBackend, :log_name}]
 
-config :logger, :debug,
+config :logger, :log_name,
   path: "log/debug.log",
   level: :debug,
-  format: "$dateT$timeZ [$level] $message\n"
-
-config :logger, :error,
-  path: "log/error.log",
-  level: :error,
-  format: "$dateT$timeZ [$level] $message\n"
+  metadata: [:file, :line],
+  json_encoder: Jason,
+  uuid: true
